175a9e9ff4ab78864824ef2e5c8b3814b5de9dfa
Andrius Simanaitis <eragonas5@gmail.com>
2021-10-20 23:17:40 +0300

    [FEAT] added ConsoleReader and FrequencyPrinter to main class

--- src/lt/simanaitis/andrius/Task1.java
+++ src/lt/simanaitis/andrius/Task1.java
@@ -1,7 +1,24 @@
 package lt.simanaitis.andrius;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Map;
+
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        GraphFormatter graphFormatter = new GraphFormatter();
+        NumberFormatter numberFormatter = new NumberFormatter();
+        FrequencyPrinter frequencyPrinter = new FrequencyPrinter(graphFormatter, numberFormatter);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        FrequencyCalculator frequencyCalculator = new FrequencyCalculator();
+        ConsoleReader consoleReader = new ConsoleReader(reader, frequencyCalculator);
+
+        try {
+            Map<Integer, Integer> frequencyMap = consoleReader.readConsole();
+            frequencyPrinter.print(frequencyMap);
+        }catch (Exception e){
+            System.err.println(e.getMessage());
+        }
     }
 }
